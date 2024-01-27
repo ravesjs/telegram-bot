5dcadb36cf86d032afa81ceb398e48ef4ff2d274
raves <hswatter@mail.ru>
2024-01-27 14:54:49 +0300

    Update mongo.js

--- src/mongo.js
+++ src/mongo.js
@@ -7,11 +7,7 @@ const products = data.products
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(config.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(config.MONGODB_URL);
     const db = mongoose.connection;
     db.on('error', (error) => {
       console.error('Ошибка подключения к MongoDB:', error);
